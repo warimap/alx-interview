498a29d876c0965d0ddf1e0f0c462a4c83b6cea8
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-10-25 15:53:49 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
-
 import sys
+import re
 
+def print_stats(file_size, status_codes):
+    print(f"File size: {file_size}")
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
-i = 0
-sum_file_size = 0
-status_code = {'200': 0,
-               '301': 0,
-               '400': 0,
-               '401': 0,
-               '403': 0,
-               '404': 0,
-               '405': 0,
-               '500': 0}
+def main():
+    file_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    count = 0
 
-try:
-    for line in sys.stdin:
-        args = line.split(' ')
-        if len(args) > 2:
-            status_line = args[-2]
-            file_size = args[-1]
-            if status_line in status_code:
-                status_code[status_line] += 1
-            sum_file_size += int(file_size)
-            i += 1
-            if i == 10:
-                print('File size: {:d}'.format(sum_file_size))
-                sorted_keys = sorted(status_code.keys())
-                for key in sorted_keys:
-                    value = status_code[key]
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                i = 0
-except Exception:
-    pass
-finally:
-    print('File size: {:d}'.format(sum_file_size))
-    sorted_keys = sorted(status_code.keys())
-    for key in sorted_keys:
-        value = status_code[key]
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    try:
+        for line in sys.stdin:
+            match = re.match(r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)', line)
+            if match:
+                status_code = int(match.group(3))
+                file_size += int(match.group(4))
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                
+                count += 1
+                if count % 10 == 0:
+                    print_stats(file_size, status_codes)
+        
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+        sys.exit(0)
+
+    print_stats(file_size, status_codes)
+
+if __name__ == "__main__":
+    main()
+
